f99281db11d10c6960e0289f0bfd3a12bcecfad9
PavelZaika <zpk.loki@gmail.com>
2018-09-09 17:00:39 +0300

    first_test

--- src/test/java/com/facebook/FirstTest.java
+++ src/test/java/com/facebook/FirstTest.java
@@ -8,7 +8,7 @@ public class FirstTest extends WebDriverSettings {
     @Test
 
 
-    public void firstTest1() {
+    public void firstTest1() { //test
 
         driver.get("https://www.facebook.com/");
         System.out.println("Open");
